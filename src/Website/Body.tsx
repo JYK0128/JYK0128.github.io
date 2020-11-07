5b8a119bab4f3b75e3d91cba93937bf5f25842f1
Kim Jinyong <wlsyong90@gmail.com>
2020-11-08 02:41:33 +0900

    web v0.01
    init first

---
+++ src/Website/Body.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
+import Board from './Page/Board'
+import Home from './Page/Home'
+import Editor from './Page/Editor'
+import Test from "./Modal/Test";
+
+type Props = {};
+type State = {};
+
+export default class Body extends React.Component<Props, State> {
+    render() {
+        return (
+            <Switch>
+                <Route path={"/"} exact component={Home}/>
+                <Route path={"/board"} exact component={Board}/>
+                <Route path={"/editor"} exact component={Editor}/>
+                <Route path={"/test"} exact component={Test}/>
+                <Redirect path={"*"} to={"/"}/>
+            </Switch>
+        );
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Website from "./Website/Website";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <App />,
+    <Website />,
     document.getElementById('root')
 );
